2373ab3e8ebc3e0844b5ed3e197de7d6a8eb7566
Timothy Cyrus <github@tcyr.us>
2019-04-24 19:44:10 -0400

    Some Changes

--- SEIR.c
+++ SEIR.c
@@ -17,9 +17,7 @@
 #include <pthread.h>
 #include <time.h>
 
-//#define BGQ 1 // when running BG/Q, comment out when testing on mastiff
-
-#ifdef BGQ
+#ifdef __bgq__
 #include <hwi/include/bqc/A2_inlines.h>
 #define PROC_FREQ 1600000000.0
 #else
@@ -57,8 +55,10 @@ const char * stateNames[] = { "B", "F", "S", "E", "I", "R", "D", "W" }; // In ca
 /* Global Vars *************************************************************/
 /***************************************************************************/
 
+/*
 int world_size = -1,
 world_rank = -1;
+*/
 
 double g_time_in_secs = 0,
 io_time_in_secs = 0;
@@ -72,8 +72,7 @@ const double g_processor_frequency = PROC_FREQ;
 
 const double threshold = 0.75;
 
-char * filename = OUT_FILE;
-
+/*
 int* recv_above;
 int* recv_below;
 int* send_above;
@@ -84,24 +83,23 @@ MPI_Request recieveUp,
 recieveDown,
 sendUp,
 sendDown;
+*/
 
 typedef struct {
 	unsigned int time_in_state;
 	state my_state;
-	int age; //May enum? but need ranges of values so may not work
-
+	unsigned int age;
 } Person;
 
-typedef struct { //TODO Decide if using threads or ranks, if only threads, wont need width and height for grid
+typedef struct {
 	size_t width;
 	size_t height;
 	Person** current;
 	Person** previous;
-
-} BoardChunk;
+} Board;
 
 // May be a way to store the updated values before we finish going through all values
-BoardChunk bc;
+Board bc;
 
 /***************************************************************************/
 /* Function Decs ***********************************************************/
@@ -115,14 +113,13 @@ void Init_person(Person * person) {
 
 // Handles board initialization
 // BoardChunk is clear on initialization
-void Init_board(BoardChunk* b, size_t width, size_t height) {
+void Init_board(Board* b, size_t width, size_t height) {
 	b->width = width;
 	b->height = height;
 	b->current = calloc(height, sizeof(Person*));
 	b->previous = calloc(height, sizeof(Person*));
 
 	for (size_t i = 0; i < height; i++) {
-
 		b->current[i] = calloc(width, sizeof(Person));
 		b->previous[i] = calloc(width, sizeof(Person));
 
@@ -218,19 +215,18 @@ void infect_people(BoardChunk* b) {
 * Parameters: x and y coordinates of target cell
 * Output: number of infected cells adjacent to the target cell
 * We will make the border of the world be a border, so this function will 
-*	never recieve a cell on the edge of the grid
+* never recieve a cell on the edge of the grid
 */
 size_t get_infected_neighbors(BoardChunk* b, int x, int y) {
-	//We will not be using wrap around for this version
+	// We will not be using wrap around for this version
 	size_t count = 0;
 	int left = x - 1,
 		right = x + 1,
 		up = y + 1,
 		down = y - 1;
-	
 
-	//We only care about infected cells in state I,
-	//	infected cells in state W are non-infectious
+	// We only care about infected cells in state I,
+	// infected cells in state W are non-infectious
 	if (b->current[left][y].my_state == I)
 		count++;
 	if (b->current[left][up].my_state == I)
@@ -258,8 +254,8 @@ size_t get_infected_neighbors(BoardChunk* b, int x, int y) {
 * Output: The value of the state that will succeed this state
 * 
 */
-void next_state(BoardChunk * b, int x, int y) {
-	Person * current_person = &b->current[x][y];
+void next_state(Board* b, int x, int y) {
+	Person* current_person = &b->current[x][y];
 	//Check current persons state to decide action
 	if (current_person->my_state == S) { // If suceptible, count infected neighbors, decide if exposed
 		int infectedNeighbors = get_infected_neighbors(b, x, y);
@@ -268,8 +264,6 @@ void next_state(BoardChunk * b, int x, int y) {
 			current_person->my_state = E;
 		}
 	}
-
-
 }
 
 void* rowTick(void* argp) {
